00ca9c7e82fb29c83b67ac8cc7c9af00e394f8f1
Pinho <paispinho@gmail.com>
2023-08-25 14:10:53 -0300

    Finalização de pokedex via DIO course

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,25 +16,46 @@
 //     });
 // transformando as funções acima em arrow function
 
-function convertPokemonToLi(pokemon){
-    return`
-        <li class="pokemon">
-            <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-                </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}"/>
-            </div>    
-        </li>
-    `
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const maxRecords = 1281;
+const limit = 6;
+let offset = 0;
+
+
+function loadPokemonItens(offset, limit){
+    pokeAPI.getPokemons(offset, limit ).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>`
+                <li class="pokemon ${pokemon.type}" >
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join("")}
+                        </ol>
+                    <img src="${pokemon.photo}"
+                    alt="${pokemon.name}"/>
+                    </div>    
+                </li>
+            `).join('');
+        pokemonList.innerHTML += newHtml;
+    });    
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItens(offset,limit);
+loadMoreButton.addEventListener('click', () => {
+    
+    offset += limit
+    
+    const qtdRecordNextPage = offset + limit
+    if(qtdRecordNextPage>= maxRecords){
+    const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
 
-pokeAPI.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('');
-    pokemonList.innerHTML = newHtml;
-});    
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    }else{
+        loadPokemonItens(offset, limit);
+    }
+
+})
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,25 @@
 const pokeAPI = {}
 
-pokeAPI.getPokemons =  ( offset = 0, limit = 10) => {
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types
+    pokemon.types = types 
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+//Fazer uma nova requisião para a propriedade "url" para vir os detalhes do pokemon
+pokeAPI.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+
+}
+pokeAPI.getPokemons =  ( offset = 0, limit = 6) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     
     return fetch(url)
@@ -9,12 +28,10 @@ pokeAPI.getPokemons =  ( offset = 0, limit = 10) => {
     .then((jsonBody)=> jsonBody.results)
     //Quando minha function só tem uma linha no corpo eu não preciso escreve-lo
     //Retorna a promisse do then anterior
-    .catch ((error) => console.error(error))
+    ///agora eu quero que para cada retorno(pokemon) tenha uma nova promisse que traga a propriedade "url"
+    //conforme função armazenada no novo metodo acima.
+    .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) => pokemonDetails)
 }
 
-Promise.all([
-    fetch(`https://pokeapi.co/api/v2/pokemon/1`),
-    fetch(`https://pokeapi.co/api/v2/pokemon/2`),
-    fetch(`https://pokeapi.co/api/v2/pokemon/3`),
-    fetch(`https://pokeapi.co/api/v2/pokemon/4`)
-]).then((results) => console.log(results))
